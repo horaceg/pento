2c733724f5e04e430187bccdc475bfaad6083fe5
Horace Guy <horace.guy.k@gmail.com>
2023-07-19 19:03:29 +0200

    add phx.gen.auth

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,24 +1,36 @@
 defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
+  alias Pento.Accounts
 
-  def mount(_params, _session, socket) do
+  def mount(_params, session, socket) do
     answer = :rand.uniform(10)
     IO.puts(answer)
-    {:ok, assign(socket, score: 0, message: "Make a guess:", answer: answer)}
+
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       answer: answer,
+       session_id: session["live_socket_id"],
+     )}
   end
 
   def render(assigns) do
     ~H"""
-    <h1>Your score: <%=  @score %></h1>
+    <h1>Your score: <%= @score %></h1>
     <h2>
       <%= @message %>
     </h2>
     <h2>
       <%= for n <- 1..10 do %>
-        <.link  href='#' phx-click="guess" phx-value-number={n} >
+        <.link href="#" phx-click="guess" phx-value-number={n}>
           <%= n %>
         </.link>
       <% end %>
+      <pre>
+        <%= @current_user.email %>
+        <%= @session_id %>
+      </pre>
     </h2>
     """
   end
